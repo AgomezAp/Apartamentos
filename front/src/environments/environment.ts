27d617fb73ab3d3b680b0ad321aa6d9cde0ecf34
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2026-02-02 08:18:58 -0500

    Cambios en el environment

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,8 +1,8 @@
-/* export const environment = {
-  production: false,
-  apiUrl: 'https://echoesoftheoracle.com/api'
-}; */
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3010/api'
+  apiUrl: 'https://numerologiapanama.com/api'
 };
+/* export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3010/api'
+}; */
