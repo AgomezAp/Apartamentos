e728c79745ce0e7012e37c2af6957e51ba1e4492
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2026-02-10 12:25:15 -0500

    Cambios logo y environment

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,8 +1,9 @@
-/* export const environment = {
+export const environment = {
   production: false,
   apiUrl: 'https://api.gestionmobiliariaap.com/api'
-}; */
-export const environment = {
+};
+/* export const environment = {
   production: false,
   apiUrl: 'http://localhost:3010/api'
 };
+ */
